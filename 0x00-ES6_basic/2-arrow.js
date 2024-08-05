8db2f8aea74841de47538129c8195d8c09635343
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-08-05 13:23:09 +0100

    refactor: use arrow function for adding neighborhoods

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -2,7 +2,6 @@ export default function getNeighborhoodsList() {
     this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
   
     const self = this;
-    /* eslint-disable no-unused-vars */
     this.addNeighborhood = (newNeighborhood) => {
       self.sanFranciscoNeighborhoods.push(newNeighborhood);
       return self.sanFranciscoNeighborhoods;
