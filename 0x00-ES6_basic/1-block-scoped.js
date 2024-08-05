99daa09b93b7f62b09e1069dcb90452dcd82ba18
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-08-05 13:14:07 +0100

    refactor: improve block-scoped variable declaration

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,6 +3,7 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
+    /* eslint-disable no-unused-vars */
     const task = true; // This task is scoped to the if block
     const task2 = false; // This task2 is scoped to the if block
   }
