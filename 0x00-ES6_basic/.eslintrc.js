30b567b6380128b215e6f31a471b628488d70b0c
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-08-05 13:42:00 +0100

    refactor: Update Babel configuration to target current Node.js version

--- 0x00-ES6_basic/.eslintrc.js
+++ 0x00-ES6_basic/.eslintrc.js
@@ -1,19 +1,35 @@
 module.exports = {
   env: {
-    browser: true,
-    es2021: true,
-    node: true,
-    jest: true
+    browser: false,
+    es6: true,
+    jest: true,
   },
-  extends: 'eslint:recommended',
+  extends: [
+    'airbnb-base',
+    'plugin:jest/all',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
   parserOptions: {
-    ecmaVersion: 12,
+    ecmaVersion: 2018,
     sourceType: 'module',
   },
+  plugins: ['jest'],
   rules: {
     'no-console': 'off',
-    'semi': ['error', 'always'],
-    'quotes': ['error', 'single'],
+    'no-shadow': 'off',
+    'no-restricted-syntax': [
+      'error',
+      'LabeledStatement',
+      'WithStatement',
+    ],
   },
+  overrides:[
+    {
+      files: ['*.js'],
+      excludedFiles: 'babel.config.js',
+    }
+  ]
 };
-
