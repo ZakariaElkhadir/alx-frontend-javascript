777ce89378e0bd6b8900b66c5f8ad82751101139
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-08-05 22:15:25 +0100

    refactor: Use spread operator to concatenate arrays in concatArrays function

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,5 +1,4 @@
 export default function concatArrays(array1, array2, string) {
-  const strArray = string.split('');
-  const combinedArray = array1.concat(array2, strArray);
-  return combinedArray;
+  const strArray = [...array1, ...array2, ...string];
+  return strArray;
 }
