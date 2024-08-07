6f0127e7ccd8aa24de1e17dd2c5f6e72b265c707
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-08-07 15:54:02 +0100

    refactor: Update handleProfileSignup function to use Promise.allSettled for uploading photo and creating user

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,7 +1,12 @@
-import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
 
-function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), signUpUser()]);
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => results.map((result) => {
+      if (result.status === 'fulfilled') {
+        return { status: result.status, value: result.value };
+      }
+      return { status: result.status, value: result.reason.toString() };
+    }));
 }
-export default handleProfileSignup;
