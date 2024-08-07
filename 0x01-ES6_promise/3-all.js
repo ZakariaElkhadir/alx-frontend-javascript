beb82c1dc40af935816cf5c4138c34fac40a795d
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-08-07 14:50:51 +0100

    refactor: Update handleProfileSignup function to remove duplicate console.log statement

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,14 @@
 import { uploadPhoto, createUser } from './utils';
 
+/**
+ * Handles the signup process for a user's profile.
+ *
+ * @returns {Promise} A promise that resolves when both the photo upload
+ * and user creation are completed.
+ * @throws {Error} If the signup system is offline.
+ */
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-    .then((values) => console.log(`${values[0].body} ${values[0].body} ${values[1].firstName} ${values[1].lastName}`))
+    .then((values) => console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`))
     .catch(() => console.log('Signup system offline'));
 }
