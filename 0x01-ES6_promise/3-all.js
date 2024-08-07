d26f1da9fbdfc92fd496c4d8c211d6c196779e69
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-08-07 14:47:45 +0100

    refactor: Update handleProfileSignup function to use Promise.all for uploading photo and creating user

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,12 +1,5 @@
 import { uploadPhoto, createUser } from './utils';
 
-/**
- * Handles the profile signup process.
- *
- * @returns {Promise} A promise that resolves when both the photo
- * upload and user creation are completed.
- * @throws {Error} If the signup system is offline.
- */
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((values) => console.log(`${values[0].body} ${values[0].body} ${values[1].firstName} ${values[1].lastName}`))
