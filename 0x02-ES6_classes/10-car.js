38797a70c22bffbae8581bbdd63672cb0092a0a9
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-08-10 00:34:08 +0100

    refactor: Update

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,18 @@
+/**
+ * Represents a Car.
+ * @class
+ */
+class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    const newCar = new this.constructor(this._brand, this._motor, this._color);
+    return newCar;
+  }
+}
+
+export default Car;
