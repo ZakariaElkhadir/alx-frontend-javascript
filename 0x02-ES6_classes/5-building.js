39090000c41c4c423968c930bc415f2ea24f41e4
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-08-09 16:38:00 +0100

    refactor: Update Building class with getters and setters for sqft property

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,10 +1,15 @@
-class Building{
-    constructor(sqft){
-        this._sqft = sqft
+class Building {
+    constructor(sqft) {
+        this._sqft = sqft;
     }
-    get sqft(){ return this._sqft }
-    evacuationWarningMessage(){
-        throw new Error("Warning: evacuate the building!")
+
+    get sqft() {
+        return this._sqft;
+    }
+
+    evacuationWarningMessage() {
+        throw new Error("Class extending Building must override evacuationWarningMessage");
     }
 }
+
 export default Building;
