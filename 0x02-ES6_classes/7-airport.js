a97f3d93e89bbf419af8f34b4d49f75fd3256663
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-08-09 19:14:00 +0100

    refactor: Update Airport class with toString method

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,13 @@
+class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  
+  }
+
+  toString() {
+    return `${this._code} - ${this._name}`;
+  }
+  
+}
+export default Airport;
