cf5f662e8af7efe3595b6db63afec9ec8b36af9f
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-08-12 12:17:20 +0100

    eslin fix

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,6 +1,6 @@
 const getStudentIdsSum = (students) => {
-    //accumulator starts at 0 like a temp variable
-    const result = students.reduce((accumulator, currentValue) => accumulator + currentValue.id, 0);
-    return result;
-}
+  // accumulator starts at 0 like a temp variable
+  const result = students.reduce((accumulator, currentValue) => accumulator + currentValue.id, 0);
+  return result;
+};
 export default getStudentIdsSum;
