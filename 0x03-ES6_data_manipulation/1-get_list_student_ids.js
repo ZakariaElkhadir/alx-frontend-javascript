ff812e154539949841912d91125b6fd4181bdfc1
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-08-12 11:33:23 +0100

    refactor: Update getListStudenIds function to use consistent indentation

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,9 +1,9 @@
 function getListStudenIds(arg) {
-    // Check if arg is an array
+  // Check if arg is an array
   if (!Array.isArray(arg)) {
     return [];
   }
-    // Check if all elements in the array are objects
+  // Check if all elements in the array are objects
   return arg.map((item) => item.id);
 }
 export default getListStudenIds;
