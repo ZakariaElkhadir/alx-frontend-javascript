752732d93fbe6bc8ae6410ade396991966d80158
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-08-12 11:10:44 +0100

    add comments

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,8 +1,9 @@
 function getListStudenIds(arg) {
+    // Check if arg is an array
   if (!Array.isArray(arg)) {
     return [];
   }
-
+    // Check if all elements in the array are objects
   return arg.map((item) => item.id);
 }
 export default getListStudenIds;
