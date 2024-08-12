2e83c2295ab21b88f59424944df075db55686598
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-08-12 11:32:18 +0100

    feat: Add function to get students by location

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,9 @@
+import getListStudents from './0-get_list_students.js';
+
+function getStudentByLocation() {
+    const students = getListStudents();
+    const result = students.filter((student) => student.id === 1 || student.id === 5);
+    return result;
+}
+
+export default getStudentByLocation;
