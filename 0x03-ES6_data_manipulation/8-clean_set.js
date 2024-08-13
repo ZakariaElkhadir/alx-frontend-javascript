873ebae9e65b379156551bfa94df267656e4f58f
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-08-13 11:10:48 +0100

    Refactor cleanSet function to improve readability and maintainability

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,12 @@
 /**
- * Cleans a set by filtering out values that do not start with a given string and returns a hyphen-separated string of the remaining values.
+ * Cleans a set by filtering out values that do not start with
+ * a given string and returns a hyphen-separated string of the remaining values.
  *
  * @param {Set} set - The set to be cleaned.
- * @param {string} startString - The string that the values in the set should start with.
- * @returns {string} - A hyphen-separated string of the values in the set that start with the given string.
+ * @param {string} startString - The string that
+ * the values in the set should start with.
+ * @returns {string} - A hyphen-separated string of
+ * the values in the set that start with the given string.
  */
 function cleanSet(set, startString) {
   if (!startString || typeof startString !== 'string') {
