4060b2a2b39d93c6b2840c24998464afcad49185
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-08-13 08:33:58 +0100

    feat: Update student grades by city

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,8 @@
+function updateStudentGradeByCity(students, city, newGrades) {
+    return students.filter((student) => student.location === city)
+    .map((student) => {
+        const grades = newGrades.filter((grade) => grade.studentId === student.id);
+        student.grade = grades.length ? grades[0].grade : 'N/A';
+        return student;
+    });
+}export default updateStudentGradeByCity;
