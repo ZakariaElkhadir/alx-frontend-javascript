c15e154ad45363705af7ed2c990b7e0908b17d8d
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-08-13 08:52:31 +0100

    refactor: Update updateStudentGradeByCity function to use consistent indentation

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,10 +1,11 @@
 function updateStudentGradeByCity(students, city, newGrades) {
   return students.filter((student) => student.location === city)
     .map((student) => {
-      const gradesFiltered = newGrades.filter((grade) => grade.studentId === student.id);
-      const updatedStudent = {
-        grade: gradesFiltered.length ? gradesFiltered[0].grade : 'N/A',
+      const grades = newGrades.filter((grade) => grade.studentId === student.id);
+      // return object with updated grade
+      return {
+        ...student,
+        grade: grades.length ? grades[0].grade : 'N/A',
       };
-      return updatedStudent;
     });
 } export default updateStudentGradeByCity;
